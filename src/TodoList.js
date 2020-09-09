a379019780b5efcb1b097185bc839049ef63ad0b
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-09 12:55:59 -0400

    added a key to the todo list

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Todo from './Todo'
 
-export default function TodoList() {
+export default function TodoList({todos}) {
     return (
         <div>
-            Hello World
+            {todos.map(todo => {
+                return <Todo key={todo} todo={todo} />
+            })}
         </div>
     )
 }
